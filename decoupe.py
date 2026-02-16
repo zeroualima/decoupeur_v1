aa5526b61d770144a663678e04b3aa73b4b33625
test.0 <test.0@test.com>
2026-02-16 11:36:24 +0100

    optimisation

--- decoupe.py
+++ decoupe.py
@@ -5,28 +5,6 @@ from minigeo.affichable import affiche
 from minigeo.utils import multiples_entre
 
 
-# fct de tri rapide en (QuickSort in place) pour tris des segments selon leurs hauteurs
-
-def partition(segments_h, inf, sup):
-    pivot = segments_h[sup][1]
-    i = inf - 1
-    
-    for j in range(inf, sup):
-        if segments_h[j][1] <= pivot:
-            i += 1
-            segments_h[i], segments_h[j] = segments_h[j], segments_h[i]
-    
-    segments_h[i + 1], segments_h[sup] = segments_h[sup], segments_h[i + 1]
-    return i + 1
-
-def quicksort(segments_h, inf, sup):
-    if inf < sup:
-        pivot = partition(segments_h, inf, sup)
-        quicksort(segments_h, inf, pivot - 1)
-        quicksort(segments_h, pivot + 1, sup)
-
-
-
 def decoupe(facettes, epaisseur):
     """
     renvoie un vecteur de vecteurs de segments.
@@ -34,39 +12,36 @@ def decoupe(facettes, epaisseur):
     le vecteur externe contient toutes les coupes de tranches de la plus basse (x minimal)
     a la plus haute (x maximal).
     """
+    # calcule de limite des hauteurs zmin et zmax
 
-    # Collection de tous les segments, chacun avec sa heuteur
-    segments_h = []
-    for facette in facettes :
-        zmin, zmax = facette.zmin_et_zmax()
-        hauteur = 0
-        while hauteur < zmin :
-            hauteur += epaisseur
-        while hauteur <= zmax :
-            segment = facette.intersection_plan_horizontal(hauteur)
-            if segment != () :
-                segments_h.append([segment, hauteur])
-            hauteur += epaisseur
+    # zmin, zmax = [], []
+    # for facette in facettes:
+    #     x, y = facette.zmin_et_zmax()
+    #     zmin.append(x)
+    #     zmax.append(y)
+    # zmin = min(zmin)
+    # zmax = max(zmax)
 
-    if segments_h == [] :
-        return []
+    altitudes = [facette.zmin_et_zmax() for facette in facettes]
+    zmin = min([altitude[0] for altitude in altitudes])
+    zmax = max([altitude[1] for altitude in altitudes])
 
-    # Tris des segments selon leurs hauteurs ( O(nlog(n)) yy)
-    # quicksort(segments_h, 0, len(segments_h) - 1)
-    segments_h.sort(key=lambda x: x[1])
+    # calcule de toutes les hauteurs de tranches
+    hauteurs = multiples_entre(zmin, zmax, epaisseur)
 
-    # Ramassage des segments de meme hauteur
-    tranches = [[segments_h[0][0]]]
-    EPS = 1e-9
-    for i in range(1, len(segments_h)) :
-        # Pour éviter le problème de comparaison entre les floats
-        # on compare avec une précision de 1e-9
-        if abs(segments_h[i][1] - segments_h[i - 1][1]) < EPS:
-            tranches[len(tranches) - 1].append(segments_h[i][0])
-        else :
-            tranches.append([segments_h[i][0]])
+    # construction de tranches
+    tranches = []
+    for hauteur in hauteurs:
+        segments_tranche = []
+        for facette in facettes:
+            zmin, zmax = facette.zmin_et_zmax()
+            if zmin <= hauteur <= zmax:
+                segments = facette.intersection_plan_horizontal(hauteur)
+                segments_tranche.extend(segments)
+        tranches.append(segments_tranche)
 
     return tranches
+
 
 def main():
     if len(argv) != 3:
